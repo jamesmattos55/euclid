1f8e808210ec28a894048990ec8aeabd353a649e
James Mattos <jamesmattos55@yahoo.com>
2019-01-28 16:15:42 -0700

    Initial commit

---
+++ src/cnm/edu/deepdive/Euclid.java
@@ -0,0 +1,29 @@
+package cnm.edu.deepdive;
+
+public class Euclid {
+
+  public static long gcd(long a,long b) {
+    a = Math.abs(a);
+    b = Math.abs(b);
+    if (a == 0) {
+      if (b == 0) {
+        throw new ArithmeticException("GCD(0, 0) is undefined.");
+      }
+      return b;
+    }
+    if (b == 0) {
+      return a;
+    }
+    while (b != 0 &&& a != 0) {
+      if (a > b) {
+        a %= b;
+      } else {
+        b %= a;
+      }
+    }
+    if (a != 0) {
+    return a;
+  } else {
+      return b;
+    }
+}
